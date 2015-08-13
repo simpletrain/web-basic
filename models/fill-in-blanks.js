72205f1b938a3c996d3036527854cef2542612aa
simpletrain <zsy498761554@live.com>
2015-08-13 22:51:40 +0800

    can mark fib

--- models/fill-in-blanks.js
+++ models/fill-in-blanks.js
@@ -1,4 +1,5 @@
 var Topic = require('./topic');
+var _ = require('lodash');
 
 function FillInBlanks(question,options,answer,score,input){
     Topic.call(this,'fill_in_blanks',question,options,answer,score,input);
@@ -8,7 +9,17 @@ FillInBlanks.prototype = Object.create(Topic.prototype);
 FillInBlanks.prototype.constructor = FillInBlanks;
 
 FillInBlanks.prototype.mark = function(){
-    return 0;
+    var score = 0;
+
+    if(!Array.isArray(this.input)){
+        if(this.answer.indexOf(this.input) !== -1){
+            score += this.score;
+        }
+    } else  {
+        score += _.intersection(this.input,this.answer).length * this.score;
+    }
+
+    return score;
 };
 
 
