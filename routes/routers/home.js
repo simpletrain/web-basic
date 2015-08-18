4b8e8dcc703b5d4200f823faaed40899eae3226e
simpletrain <zsy498761554@live.com>
2015-08-18 16:21:08 +0800

    delete useless require

--- routes/routers/home.js
+++ routes/routers/home.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var HomeController = require('../../controller/home-controller');
-var data = require('../../seed/data.json');
 
 
 var homeController = new HomeController();
